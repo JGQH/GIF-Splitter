1e1aa8191301669b102fac17733eab40657add7f
Juan Quispe <jquispeh63@gmail.com>
2021-01-22 19:31:55 -0500

    Adds button to save spritesheet
    -Saves original gif data
    -Adds button to locate and save the spritesheet as PNG file
    -Adds lastDir variable to change the location where to read images

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("GIF Splitter")
         self.setFixedSize(200, 200)
         self.setup()
+        self.lastDir = "D:\\"
 
     def setup(self):
         lblSearch = QLabel(self)
@@ -29,16 +30,34 @@ class MainWindow(QMainWindow):
 
         qptPreview = QPlainTextEdit(self)
         qptPreview.move(10, 60)
-        qptPreview.resize(180, 100)
+        qptPreview.resize(180, 95)
+
+        btnSplit = QPushButton(self)
+        btnSplit.move(50, 165)
+        btnSplit.resize(100, 25)
+        btnSplit.setText("Get Spritesheet")
+        btnSplit.setEnabled(False)
 
         def searchGif():
-            location = QFileDialog.getOpenFileName(None, "Open GIF file", "D:\\", "GIF files (*.gif)")[0]
+            location = QFileDialog.getOpenFileName(None, "Open GIF file", self.lastDir, "GIF files (*.gif)")[0]
             if(location == ""): return
 
+            self.lastDir = path.dirname(location)
             btnSearch.setText("[%s]" % path.basename(location))
             qptPreview.setPlainText(self.processGif(location))
-
+            btnSplit.setEnabled(True)
         btnSearch.clicked.connect(searchGif)
+
+        def splitGif():
+            location = QFileDialog.getSaveFileName(None, "Save Spritesheet", path.join(self.lastDir, "Sprite_From_GIF"), "PNG file (*.png)")[0]
+            if(location == ""): return
+
+            output = Image.new('RGBA', (self.final_width, self.height), (0, 0, 0, 0))
+            for i in range(len(self.frames)):
+                frame = self.frames[i]
+                output.paste(frame, (i * self.width, 0))
+            output.save(location)
+        btnSplit.clicked.connect(splitGif)
 
     def processGif(self, location:str)->str:
         raw_gif = Image.open(location)
@@ -55,11 +74,13 @@ class MainWindow(QMainWindow):
             pass
         
         self.frames = frames
+        self.width, self.height = raw_gif.size
+        self.final_width = self.width * len(frames)
         return "-Total Frames: {frames}\n-Frame Size: {frame_width}x{frame_height}\n-Spritesheet size:{total_width}x{frame_height}".format(
             frames=len(frames),
-            frame_width=raw_gif.size[0],
-            frame_height=raw_gif.size[1],
-            total_width=(raw_gif.size[0] * len(frames))
+            frame_width=self.width,
+            frame_height=self.height,
+            total_width=self.final_width
         )
 
 if __name__ == "__main__":
